3a90a81949aeb67179dd7a38b7edc16fb673da67
Pholey <Pholey@users.noreply.github.com>
2015-06-12 02:05:00 -0700

    Add MongoDatabaseAdapter

--- chatterbot/adapters/storage/__init__.py
+++ chatterbot/adapters/storage/__init__.py
@@ -1,2 +1,8 @@
 from .database import DatabaseAdapter
 from .jsondatabase import JsonDatabaseAdapter
+from .mongodb import MongoDatabaseAdapter
+
+__all__ = [
+  "JsonDatabaseAdapter",
+  "MongoDatabaseAdapter"
+]
